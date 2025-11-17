b8540756e131508aa392de38ce42e412d53139ca
Tobias Teichner <webmaster@teichner.biz>
2025-11-17 10:16:10 +0100

    CHG: Switch to an approach with svg

--- src/draw.js
+++ src/draw.js
@@ -15,29 +15,9 @@ class HexagonalLayoutManager extends HTMLElement {
 
     _opts = null;
 
-    TAU = 2 * Math.PI;
-
-    pickRandom(arr) {
-        return arr[Math.floor(Math.random() * arr.length)];
-    }
-
     get cells() {
         return this._cells;
     };
-
-    _toPoint(x, y) {
-        return ({x, y});
-    };
-
-    _polyPath3(points = []) {
-        const [{x : startX, y : startY}] = points;
-        this.ctx.beginPath();
-        this.ctx.moveTo(startX, startY);
-        points.forEach(({x, y}) => {
-            this.ctx.lineTo(x, y);
-        });
-        this.ctx.closePath();
-    }
 
     drawHQ(x, y, options = {}) {
         x *= 1;
@@ -51,83 +31,103 @@ class HexagonalLayoutManager extends HTMLElement {
 
         const opts = {...JSON.parse(JSON.stringify(this._opts)), ...options};
         opts.fillStyle = this._generateColor(110);
-        this.ctx.fillStyle = opts.fillStyle;
 
         coords.forEach(row => {
             row.forEach(cord => {
                 const cell = this._cells.find(c => {
-                    return c.x === x + cord[0] && c.y === y + cord[1];
+                    return c.row === x + cord[0] && c.col === y + cord[1];
                 });
-                this.drawPoly(cell.origin, this.points, opts, cell.x, cell.y);
+                this._drawPoly(cell.polygon, null, opts);
             });
         });
     }
 
-    drawGrid(x, y, w, h, options = {}) {
+    drawGrid(cols, rows, options = {}) {
         const opts = {...this.defaultGridOptions, ...options};
         this._opts = JSON.parse(JSON.stringify(opts));
-
-        this.points = this.createPoly(opts);
-        opts.diameter = opts.radius * 2;
-
         this._cells = [];
 
-        for (let gy = y; gy < y + h; gy++) {
-            for (let gx = x; gx < x + w; gx++) {
-                this.ctx.fillStyle = opts.randomColors ? this.pickRandom(opts.randomColors) : opts.fillStyle;
-                const origin = this._gridToPixel(gx, gy, opts);
-                this.drawPoly(origin, this.points, opts, gx, gy);
+        let x, y, row, col;
+        for (col = 0; col < cols; col += 1) {
+            for (row = 0; row < rows; row += 1) {
+                const offset = (Math.sqrt(3) * opts.radius) / 2;
+                x = opts.radius * 2 + offset * col * 2;
+                y = opts.radius * 2 + offset * row * Math.sqrt(3);
+
+                if (row % 2 !== 0) x += offset;
+
+                const polygon = this._drawPoly(x, y, opts);
+
                 this._cells.push({
-                    x : gx,
-                    y : gy,
-                    origin
+                    x, y, row, col, polygon
                 });
+                this.ctx.appendChild(polygon);
+
+                if ((col + row) % 7 === 0) {
+                    const text = `${row}:${col}`;
+                    const textOffset = [
+                        0, -6, -6, -6, -7, -9, -9, -10, -10
+                    ];
+                    this._addLabelText(polygon, text, textOffset[text.length]);
+                }
             }
         }
 
         const event = new CustomEvent("after-draw", {
-            detail: {
+            detail : {
                 cells : this._cells
             }
         });
         this.dispatchEvent(event);
     }
 
-    drawPoly(origin, points, opts, gx = 0, gy = 0) {
-        this.ctx.strokeStyle = opts.strokeStyle;
-        this.ctx.save();
-        this.ctx.translate(origin.x, origin.y);
-        this._polyPath3(points);
-
-        this.ctx.font = "9px serif";
-        const text = `${gx}:${gy}`;
-        const offset = [
-            0, -6, -6, -6, -7, -9, -9, -10, -10
-        ];
-        this.ctx.fillText(text, offset[text.length], 3);
-
-        this.ctx.restore();
-        if (opts.lineWidth) this.ctx.lineWidth = opts.lineWidth;
-        if (opts.fillStyle || opts.randomColors) this.ctx.fill();
-        if (opts.strokeStyle) this.ctx.stroke();
-    }
-
-    toPolarCoordinate(centerX, centerY, radius, angle) {
-        return ({
-            x : centerX + radius * Math.cos(angle),
-            y : centerY + radius * Math.sin(angle)
-        });
-    }
-
-    createPoly(opts, points = []) {
-        const
-            {inset, radius, sides} = opts,
-            size = radius - inset,
-            step = this.TAU / sides;
-        for (let i = 0; i < sides; i++) {
-            points.push(this.toPolarCoordinate(0, 0, size, step * i));
+    _drawPoly(x, y, opts) {
+        let polygon;
+        if (x instanceof SVGPolygonElement) {
+            polygon = x;
+            polygon.style.fill = opts.fillStyle ? opts.fillStyle : 'white';
+        } else {
+            polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+            polygon.style.fill = opts.fillStyle ? opts.fillStyle : 'white';
+            polygon.style.stroke = 'black';
+            polygon.style.strokeWidth = '2px';
+            polygon.setAttribute('points', this._hexPoints(x, y, opts.radius));
+            polygon.addEventListener('click', function(event) {
+                console.log(event.target);
+            }, false);
         }
-        return points;
+
+        return polygon;
+    }
+
+    _addLabelText(polygon, labelText) {
+        let bbox = polygon.getBBox();
+        let x = bbox.x + bbox.width / 2;
+        let y = bbox.y + bbox.height / 2;
+
+        // Create a <text> element
+        let textElem = document.createElementNS(polygon.namespaceURI, "text");
+        textElem.setAttribute("x", x);
+        textElem.setAttribute("y", y);
+        textElem.setAttribute("text-anchor", "middle");
+        textElem.classList.add("label-text");
+        textElem.textContent = labelText;
+        // Add this text element directly after the label background path
+        polygon.after(textElem);
+    }
+
+    _hexPoints(x, y, radius) {
+        const points = [];
+        for (let theta = 0; theta < Math.PI * 2; theta += Math.PI / 3) {
+            let pointX, pointY;
+
+            pointX = x + radius * Math.sin(theta);
+            pointY = y + radius * Math.cos(theta);
+
+            points.push(pointX + ',' + pointY);
+        }
+
+        return points.join(' ');
     }
 
     _generateColor(count, saturation = 1.0, lightness = 0.5, alpha = 1.0) {
@@ -139,61 +139,28 @@ class HexagonalLayoutManager extends HTMLElement {
         ].join(', ')})`
     }
 
-    generateColors(count, saturation = 1.0, lightness = 0.5, alpha = 1.0) {
-        return Array.from({length : count}, (_, i) =>
-            `hsla(${[
-                Math.floor(i / count * 360),
-                `${Math.floor(saturation * 100)}%`,
-                `${Math.floor(lightness * 100)}%`,
-                alpha
-            ].join(', ')})`)
-    }
-
-    gridMeasurements(opts) {
-        const
-            {diameter, inset, radius, sides} = opts,
-            edgeLength = Math.sin(Math.PI / sides) * diameter,
-            gridSpaceX = diameter - edgeLength / 2,
-            gridSpaceY = Math.cos(Math.PI / sides) * diameter,
-            gridOffsetY = gridSpaceY / 2;
-        return {
-            diameter,
-            edgeLength,
-            gridSpaceX,
-            gridSpaceY,
-            gridOffsetY
-        };
-    }
-
-    _gridToPixel(gridX, gridY, opts) {
-        const m = this.gridMeasurements(opts);
-        return this._toPoint(
-            Math.floor(gridX * m.gridSpaceX),
-            Math.floor(gridY * m.gridSpaceY + (gridX % 2 ? m.gridOffsetY : 0))
-        );
-    }
-
     draw() {
         // get config from attributes
         const width = (this.getAttribute('width')) ? this.getAttribute('width') * 1 : 500;
         const height = (!this.getAttribute('height') && width) ? width : this.getAttribute('height') * 1 || 500;
-        const random = this.getAttribute('random') || false;
-        const fillStyle = this.getAttribute('fillStyle') || '';
 
-        this._root.innerHTML = `<canvas id="drawing" width="${width}" height="${height}" class="canvas"></canvas>`;
+        this._root.innerHTML = `<svg id="drawing" version="1.1" width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg"></svg>`;
+
+        const style = document.createElement('style');
+        style.innerHTML = `.label-text {
+            font-size: 8px;
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+        }`;
+        this._root.appendChild(style);
 
         // count the cells fitting into it
-        const radius = 20;
-        const inset = 2;
-        const w = Math.floor(width / (radius * 2 + inset));
-        const h = Math.floor(height / (radius * 2 + inset));
+        const radius = 15;
+        const w = Math.floor(width / (radius * 2));
+        const h = Math.floor(height / (radius * 2));
 
-        this.ctx = this._root.querySelector('#drawing').getContext('2d');
-        this.drawGrid(1, 1, w, h, {
-            radius : radius,
-            inset : inset,
-            randomColors : (random) ? this.generateColors(random) : null,
-            fillStyle : fillStyle
+        this.ctx = this._root.querySelector('#drawing');
+        this.drawGrid(w, h, {
+            radius : radius
         });
     }
 
